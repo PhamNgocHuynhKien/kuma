4d52c5575ce4b9aca55f14c94743e3473cb6e577
HuynhKien <kienpnh@gmail.com>
2019-08-01 14:01:49 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AsideComponent } from './aside/aside.component';
+import { NavComponent } from './nav/nav.component';
+import { SectionComponent } from './section/section.component';
+import { MainComponent } from './main/main.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent,
+    NavComponent,
+    SectionComponent,
+    MainComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.sass']
+})
+export class NavComponent implements OnInit {
+  navh: any;
+  constructor() { }
+
+  ngOnInit() {
+    // console.log(navh);
+    // var navh = document.getElementById("nav-menu").offsetTop;
+    // if( navh = 0) {
+    //   document.getElementById("nav-menu").classList.add("navbar-bg");
+    // }
+    window.addEventListener("scroll", this.myFunction);
+  }
+
+  myFunction() {
+    console.log('ạdhakhdkad');
+    this.navh = document.getElementById("nav-menu").offsetTop;
+    console.log(this.navh);
+    if( this.navh > 200) {
+      document.getElementById("nav-menu").classList.add("navbar-bg");
+    } 
+    if( this.navh <200){
+      document.getElementById("nav-menu").classList.remove("navbar-bg");
+    }
+  
+}
+  
+}
